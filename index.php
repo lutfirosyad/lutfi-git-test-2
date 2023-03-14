fc330affd5e5c85eedf412b649b39d362f6b5fd8
lutfirosyad <lutfimarionette@gmail.com>
2023-03-14 20:50:26 +0700

    menambahkan list pada file index.php

--- index.php
+++ index.php
@@ -8,6 +8,15 @@
 </head>
 <body>
     <h1>hello world!</h1>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem asperiores quas nobis quis optio velit ab cupiditate eum voluptas impedit voluptates natus obcaecati, excepturi consectetur omnis, reiciendis dicta, inventore assumenda consequuntur sed molestiae laborum esse eveniet. Voluptas a vel dignissimos unde delectus! Totam fuga assumenda repudiandae perspiciatis. Vel, perspiciatis fugit!
+
+    </p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
     
 </body>
 </html>
